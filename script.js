82f3a951880fcee01fd1cb2c75a3cbd582e7fa90
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-04-13 07:16:17 -0700

    this is a simple calculator for arithmetic operation

---
+++ script.js
@@ -0,0 +1,14 @@
+const numberButton = document.querySelectorAll('[data-number]'
+)
+const dataOperation = document.querySelectorAll('[data-operation]'
+)
+const equalsButton = document.querySelectorAll('[equalsButton]'
+)
+const allClearButton = document.querySelectorAll('[AllClear]'
+)
+const DeleteButton = document.querySelectorAll('[Delete]'
+)
+const answer = document.querySelectorAll('[answer]'
+)
+const input = document.querySelectorAll('[inputButton]'
+)
